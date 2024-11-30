6c9f6217c4a2b911301c1c07184945807dcdac93
Sammy949 <urahsamuel0202@gmail.com>
2024-11-30 04:28:09 +0100

    Completed all sections of the project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { FaDiscord, FaGithub, FaTwitch, FaTwitter } from "react-icons/fa";
+
+const links = [
+  { href: "https://discord.com", icon: <FaDiscord /> },
+  { href: "https://twitter.com", icon: <FaTwitter /> },
+  { href: "https://github.com", icon: <FaGithub /> },
+  { href: "https://twitch.com", icon: <FaTwitch /> },
+];
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-screen bg-violet-300 py-4 text-blue-75">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <div className="flex text-center text-sm md:text-left">
+          &copy;
+          <a href="https://samy01.netlify.app" target="_blank" rel="noreferrer noopener">
+            <img
+              src="https://samy01.netlify.app/assets/logo-I4em_j5c.png"
+              alt="SamY Logo"
+              className="h-5 invert"
+            />
+          </a>
+        </div>
 
-export default Footer
+        <div className="flex justify-center gap-4 md:justify-start">
+          {links.map((link) => (
+            <a
+              key={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              href={link.href}
+              className="text-blue-75 transition-color duration-500 ease-in-out hover:text-white"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
